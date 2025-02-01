e7be62f5a1f8dcddc634fffb281703ce66c23005
Andrea Bragante <andrea.bragante00@gmail.com>
2025-02-01 22:18:49 +0100

    tunnels!

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,6 @@
-use rltk::{Rltk, RGB};
-
 use crate::Rect;
+use rltk::{Rltk, RGB};
+use std::cmp::{max, min};
 
 // so I can copy and not "move", clone programmatically, and check for type equality
 #[derive(PartialEq, Copy, Clone)]
@@ -21,13 +21,31 @@ fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
     }
 }
 
+fn apply_htunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+    for x in min(x1, x2)..=max(x1, x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+fn apply_vtunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+    for y in min(y1, y2)..=max(y1, y2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
     let mut map = vec![TileType::Wall; 80 * 50];
 
-    let room1: Rect = Rect::new(20, 10, 10, 10);
-    let room2: Rect = Rect::new(60, 20, 10, 10);
+    let room1: Rect = Rect::new(20, 15, 10, 10);
+    let room2: Rect = Rect::new(35, 15, 10, 10);
     apply_room_to_map(&room1, &mut map);
     apply_room_to_map(&room2, &mut map);
+    apply_htunnel(&mut map, 30, 35, 18);
 
     map
 }
