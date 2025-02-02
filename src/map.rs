5d3c67af6ace14b89bd3b23771f9701809815044
Andrea Bragante <andrea.bragante00@gmail.com>
2025-02-02 13:52:26 +0100

    stupid simple rooms creation

--- src/map.rs
+++ src/map.rs
@@ -40,12 +40,32 @@ fn apply_vtunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
 
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
     let mut map = vec![TileType::Wall; 80 * 50];
+    let mut rooms: Vec<Rect> = Vec::new();
+    const MAX_ROOMS: i32 = 16;
+    const MIN_SIZE: i32 = 6;
+    const MAX_SIZE: i32 = 15;
 
-    let room1: Rect = Rect::new(20, 15, 10, 10);
-    let room2: Rect = Rect::new(35, 15, 10, 10);
-    apply_room_to_map(&room1, &mut map);
-    apply_room_to_map(&room2, &mut map);
-    apply_htunnel(&mut map, 30, 35, 18);
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for _ in 0..MAX_ROOMS {
+        let w: i32 = rng.range(MIN_SIZE, MAX_SIZE);
+        let h: i32 = rng.range(MIN_SIZE, MAX_SIZE);
+
+        let x = rng.roll_dice(1, 80 - w - 1);
+        let y = rng.roll_dice(1, 50 - h - 1);
+        let new_room = Rect::new(x, y, w, h);
+
+        // check if new room overlaps with others
+        let mut ok = true;
+        for other_room in rooms.iter() {
+            if new_room.intersect(other_room) {
+                ok = false
+            }
+        }
+        if ok {
+            apply_room_to_map(&new_room, &mut map);
+            rooms.push(new_room);
+        }
+    }
 
     map
 }
